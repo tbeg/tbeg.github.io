9ef31c679d55cef428d1057ee1d514d5f42bb207
tbeg <t.f.vandenberg@gmail.com>
2015-02-07 19:00:27 +0100

    try fix nojekyll

--- js/dojo/dojoConfig.js
+++ js/dojo/dojoConfig.js
@@ -3,7 +3,7 @@ var dojoConfig;
 	var baseUrl = location.pathname.replace(/\/[^/]+$/, "/../../../example16/js/dojo/");
 	var package_path = window.location.pathname.substring(0, window.location.pathname.lastIndexOf('/'));
 	dojoConfig = {
-		async: 0,
+		async: 1,
 		cacheBust: true,
 		// Load dgrid and its dependencies from a local copy.
 		// If we were loading everything locally, this would not
